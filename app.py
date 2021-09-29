c5d4bb35b37f6a89fd8a41532dec6d7ac4a3f7f2
Edgar Suaza <edgar1422@gmail.com>
2021-09-29 09:54:36 -0500

    ok

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:54
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'some-secret-key'
 
-if __name__ == "__main__": 
-    app.run()
+#if __name__ == "__main__": 
+    #app.run()
 
 
 # Creacion de Database a traves de las librerías
